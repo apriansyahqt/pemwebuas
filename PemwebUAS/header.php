9f3524945230f645317dda3960471cb3adede4da
apriansyahqt <1apriansyahqt1@gmail.com>
2026-01-12 09:26:57 +0700

    Update header.php

--- PemwebUAS/header.php
+++ PemwebUAS/header.php
@@ -8,9 +8,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
 </head>
-    <style>
-        .nav {background-color: cyan;}
-    </style>
+
 <body>
     <nav class="navbar navbar-expand-lg mb-4">
         <div class="container">
@@ -26,3 +24,4 @@
     </nav>
 
     <main class="container">
+
